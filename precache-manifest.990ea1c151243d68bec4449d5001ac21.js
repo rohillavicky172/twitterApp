befddc63fea19a2d8f44aeefb258642582462eed
vicky.d.kumar@capgemini.com <Welcome@1234>
2019-10-15 15:37:24 +0530

    Updates

---
+++ precache-manifest.990ea1c151243d68bec4449d5001ac21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025184a3823da314784ff82f66a207e1",
+    "url": "/twitterApp/index.html"
+  },
+  {
+    "revision": "8608e512db8209a5ba84",
+    "url": "/twitterApp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "1507329020eb3e0e390c",
+    "url": "/twitterApp/static/css/main.adef6db8.chunk.css"
+  },
+  {
+    "revision": "8608e512db8209a5ba84",
+    "url": "/twitterApp/static/js/2.ad15ef56.chunk.js"
+  },
+  {
+    "revision": "1507329020eb3e0e390c",
+    "url": "/twitterApp/static/js/main.6f9f7a86.chunk.js"
+  },
+  {
+    "revision": "db8729035fc5de7aced0",
+    "url": "/twitterApp/static/js/runtime-main.1c14fd84.js"
+  },
+  {
+    "revision": "51e57402fe21bb5b6655d61d8e71ffcc",
+    "url": "/twitterApp/static/media/Search.51e57402.png"
+  },
+  {
+    "revision": "89b775fbde902de2e648b0a4a8c5ab1c",
+    "url": "/twitterApp/static/media/no-result-image.89b775fb.jpg"
+  }
+]);
